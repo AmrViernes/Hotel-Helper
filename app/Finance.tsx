5812a677f049cfaa89bf41e61ab3f30b3a76a127
AmrViernes <amraromoro@hotmail.com>
2024-03-17 06:05:32 +0200

    feat: Finished the finance Screen

--- app/Finance.tsx
+++ app/Finance.tsx
@@ -1,23 +1,63 @@
 import { StyleSheet } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "../components/Themed";
 import { Stack } from "expo-router";
-import { tintColorPrimary, tintColorWarmBackground } from "../constants/Colors";
+import { tintColorPrimary, tintColorSecondary, tintColorWarmBackground } from "../constants/Colors";
+import axios from "axios";
+import * as secureStore from "expo-secure-store";
+import { AUTH_KEY } from "./context/AuthContext";
+import Loader from "../components/Loader";
 
 const Finance = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [amount, setAmount] = useState<string>("");
+
+  useEffect(() => {
+    const abort = new AbortController();
+
+    const fetchData = async () => {
+      const gettingAuth = await secureStore.getItemAsync(AUTH_KEY);
+      const authData = JSON.parse(gettingAuth as string);
+      try {
+        const response = await axios.get(
+          "https://actidesk.oracleapexservices.com/apexdbl/boatmob/guest/fin/balance",
+          {
+            params: {
+              P_APPID: 1,
+              P_LANGCODE: "E",
+              P_RCID: authData.RC_ID,
+            },
+          }
+        );
+        setAmount(response.data.RESPONSE[0].Message);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+    return () => abort.abort();
+  }, []);
+
   return (
     <View style={styles.container}>
-          <Stack.Screen
-            options={{
-              headerTitle: "",
-              headerShadowVisible: false,
-              headerStyle: {
-                backgroundColor: tintColorWarmBackground
-              },
-              headerTintColor: tintColorPrimary
-            }}
-          />
-      <Text>Finance</Text>
+      <Stack.Screen
+        options={{
+          headerTitle: "",
+          headerShadowVisible: false,
+          headerStyle: {
+            backgroundColor: tintColorWarmBackground,
+          },
+          headerTintColor: tintColorPrimary,
+        }}
+      />
+      
+      {loading ? <Loader /> : <>
+      <Text style={styles.title}>Your Debt Amount</Text>
+      <Text style={[styles.title, {color: tintColorSecondary}]}>{amount}</Text>
+      </>}
     </View>
   );
 };
@@ -26,6 +66,15 @@ export default Finance;
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     height: "100%",
+  },
+  title: {
+    fontSize: 30,
+    fontFamily: "Poppins",
+    textAlign: "center",
+    color: tintColorPrimary,
   },
 });
