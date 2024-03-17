de7e534edef33bda0f5aca090a5d53fa8230fe1f
AmrViernes <amraromoro@hotmail.com>
2024-03-17 06:05:26 +0200

    refactor: Reomved the types to the Types file

--- app/context/DataContext.tsx
+++ app/context/DataContext.tsx
@@ -7,73 +7,29 @@ import {
   useEffect,
   useState,
 } from "react";
+import { DataContextPropsT, DataContextValueT, HomeDataT } from "../../types/types";
 
-type HomeData = {
-  ROOM_NO: string;
-  NEXT_EVENT: string;
-  FINANCE: Array<any>;
-  PROGRAM: Array<{
-    PROG_ID: number;
-    PROG_DATE: string;
-    PROG_TIME: string;
-    PROG_TITLE: string;
-    PROG_DESCRIPTION: string;
-  }>;
-  REQUEST: Array<{
-    REQ_ID: number;
-    DEPT_NAME: string;
-    DEPT_NUMBER: number;
-    REQ_DESC: string;
-    REQ_STATUS?: string;
-    ITEM_COUNT?: number;
-  }>;
-};
+const DataContext = createContext<DataContextValueT | undefined>(undefined);
 
-interface DataContextProps {
-  children: ReactNode;
-}
-
-interface DataContextValue {
-  homeData: HomeData;
-  updateHomeData: (newData: HomeData) => void;
-  setLoadingToFalse: () => void;
-  setLoadingToTrue: () => void;
-  loading: boolean;
-}
-
-const DataContext = createContext<DataContextValue | undefined>(undefined);
-
-export const DataProvider: React.FC<DataContextProps> = ({ children }) => {
-  const [homeData, setHomeData] = useState<HomeData>({
+export const DataProvider: React.FC<DataContextPropsT> = ({ children }) => {
+  const [homeData, setHomeData] = useState<HomeDataT>({
     ROOM_NO: "",
     NEXT_EVENT: "",
     FINANCE: [],
-    PROGRAM: [
-      {
-        PROG_ID: 0,
-        PROG_TITLE: "",
-        PROG_DATE: "",
-        PROG_DESCRIPTION: "",
-        PROG_TIME: "",
-      },
-    ],
-    REQUEST: [
-      {
-        DEPT_NAME: "",
-        DEPT_NUMBER: 0,
-        REQ_ID: 0,
-        REQ_DESC: "",
-      },
-    ],
+    PROGRAM: [],
+    REQUEST: [],
   });
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
+    const abort = new AbortController()
+
     const fetchData = async () => {
       try {
         const response = await axios.get(
           "https://actidesk.oracleapexservices.com/apexdbl/boatmob/guest/home/gtData",
           {
+            signal: abort.signal,
             params: {
               P_APPID: 1,
               P_RCID: 7977,
@@ -92,9 +48,10 @@ export const DataProvider: React.FC<DataContextProps> = ({ children }) => {
     };
 
     fetchData();
+    return () => abort.abort(); 
   }, [loading]);
 
-  const updateHomeData = (newData: HomeData) => {
+  const updateHomeData = (newData: HomeDataT) => {
     setHomeData(newData);
   };
 
@@ -106,7 +63,7 @@ export const DataProvider: React.FC<DataContextProps> = ({ children }) => {
     if (!loading) setLoading(true);
   };
 
-  const value: DataContextValue = {
+  const value: DataContextValueT = {
     loading,
     setLoadingToFalse,
     setLoadingToTrue,
@@ -117,7 +74,7 @@ export const DataProvider: React.FC<DataContextProps> = ({ children }) => {
   return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
 };
 
-export const useData = (): DataContextValue => {
+export const useData = (): DataContextValueT => {
   const context = useContext(DataContext);
   if (!context) {
     throw new Error("useData must be used within a DataProvider");
