172851e1294d4459317bc084a87f7cc903b440c8
AmrViernes <amraromoro@hotmail.com>
2024-03-17 06:05:16 +0200

    Added Program Screen

---
+++ app/Program.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet} from 'react-native'
+import React from 'react'
+import { Text, View } from "../components/Themed";
+
+const Program = () => {
+  return (
+    <View>
+      <Text>Program</Text>
+    </View>
+  )
+}
+
+export default Program
+
+const styles = StyleSheet.create({})
