ad86800365cfdbebf7b3f5760cb5bbdd4e3fcd75
AmrViernes <amraromoro@hotmail.com>
2024-03-17 06:05:25 +0200

    refactor: Make removed Skeleton and added a simple spinner

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,20 +1,23 @@
 import React from "react";
-import ContentLoader from "react-content-loader";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
+import { tintColorPrimary } from "../constants/Colors";
 
-const Loader = (props: any) => (
-  <ContentLoader
-    speed={1}
-    width="100%"
-    height={150}
-    viewBox="0 0 200 100"
-    backgroundColor="#f3f3f3"
-    foregroundColor="#cccc"
-    {...props}
-  >
-    <rect x="0" y="56" rx="3" ry="3" width="410" height="6" />
-    <rect x="0" y="72" rx="3" ry="3" width="380" height="6" />
-    <rect x="0" y="88" rx="3" ry="3" width="178" height="6" />
-  </ContentLoader>
-);
+const Loader = () => {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color={tintColorPrimary} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+
+  },
+});
 
 export default Loader;
