e2d09083e6cae42e755d9ea584eca077cec85fbf
AmrViernes <amraromoro@hotmail.com>
2024-03-17 06:05:19 +0200

    feat: Added the DropdownMEnu to components

---
+++ components/DropdownMenu.tsx
@@ -0,0 +1,74 @@
+import { StyleSheet, Text, View } from 'react-native'
+import SelectDropdown from "react-native-select-dropdown";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import React from 'react'
+import { tintColorPrimary } from '../constants/Colors';
+
+type Props = {
+    data: string[],
+    title: string
+}
+
+const DropdownMenu = (props: Props) => {
+  return (
+    <SelectDropdown
+      dropdownIconPosition="right"
+      dropdownStyle={styles.dropdownMenu}
+      buttonStyle={styles.dropdownButton}
+      data={props.data}
+      defaultButtonText={props.title}
+      renderDropdownIcon={(isOpened) => {
+        return (
+          <FontAwesome
+            name={isOpened ? "chevron-up" : "chevron-down"}
+            color={tintColorPrimary}
+            size={18}
+          />
+        );
+      }}
+      onSelect={(selectedItem, index) => {
+        return selectedItem;
+      }}
+      buttonTextAfterSelection={(selectedItem, index) => {
+        // text represented after item is selected
+        // if data array is an array of objects then return selectedItem.property to render after item is selected
+        return selectedItem;
+      }}
+      rowTextForSelection={(item, index) => {
+        // text represented for each item in dropdown
+        // if data array is an array of objects then return item.property to represent item in dropdown
+        return item;
+          }}
+          buttonTextStyle={{color: tintColorPrimary, fontFamily: 'PoppinsR', fontSize: 15, opacity: 0.5, paddingTop: 2}}
+    />
+  );
+}
+
+export default DropdownMenu
+
+const styles = StyleSheet.create({
+  dropdownMenu: {
+    backgroundColor: "#E6F4F1",
+    borderRadius: 10,
+    fontFamily: "PoppinsR",
+    color: tintColorPrimary,
+  },
+  dropdownButton: {
+      fontFamily: "PoppinsR",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    backgroundColor: "#E6F4F1",
+    borderRadius: 10,
+    width: 300,
+    height: 45,
+    marginBottom: 15,
+    shadowColor: tintColorPrimary,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 1,
+    elevation: 4,
+  },
+});
