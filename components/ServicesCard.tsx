7fd327abea07e29dddfecf31110359701fc700b5
AmrViernes <amraromoro@hotmail.com>
2024-03-17 06:05:27 +0200

    feat: finished the ServicesCard component whom serve Two screens

---
+++ components/ServicesCard.tsx
@@ -0,0 +1,62 @@
+import { Dimensions, StyleSheet } from "react-native";
+import { Text, View } from "./Themed";
+import React from "react";
+import {
+  tintColorPrimary,
+  tintColorSecondary,
+} from "../constants/Colors";
+import { ServicesCardT } from "../types/types";
+
+const ServicesCard = ({ id, name, checked }: ServicesCardT) => {
+  return (
+    <View style={[styles.container, {backgroundColor: checked}]}>
+        <View style={styles.maintenanceContainer}>
+          <Text style={styles.text}>{name?.toLocaleUpperCase()}</Text>
+        </View>
+    </View>
+  );
+};
+
+export default ServicesCard;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    borderWidth: 1,
+    borderRadius: 10,
+    height: Dimensions.get("screen").height / 9,
+    marginHorizontal: 10,
+    shadowColor: tintColorPrimary,
+    textAlign: "center",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    margin: 5,
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "80%",
+    alignSelf: 'center'
+  },
+  maintenanceContainer: {
+    display: "flex",
+    flexDirection: 'row',
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    width: '100%',
+  },
+  text: {
+    color: tintColorSecondary,
+    fontFamily: "Poppins",
+    paddingHorizontal: 3,
+    textAlign: 'left',
+    fontSize: 20
+  },
+});
