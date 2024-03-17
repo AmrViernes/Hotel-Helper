374006ca420d32ec28f8813315fe35c000c5729f
AmrViernes <amraromoro@hotmail.com>
2024-03-17 06:05:16 +0200

    Added Finance Screen

---
+++ app/Finance.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+import React from 'react'
+import { Text, View } from "../components/Themed";
+
+const Finance = () => {
+  return (
+    <View>
+      <Text>Finance</Text>
+    </View>
+  )
+}
+
+export default Finance
+
+const styles = StyleSheet.create({})
