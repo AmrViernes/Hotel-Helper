6561a44ced1a275c05edcf508523a5f00a614169
AmrViernes <amraromoro@hotmail.com>
2024-03-17 06:05:18 +0200

    feat: But Redirect to Welcome Screen

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import {  ScrollView, StyleSheet } from 'react-native';
+import { Text, View } from '../components/Themed';
+import { Redirect } from 'expo-router';
+
+export default function index() {
+   return <Redirect href="/Welcome" />
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
