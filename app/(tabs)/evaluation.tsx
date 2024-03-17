5dd8565144d9fd57a9ccf24f0bda63ce2e20f0a8
AmrViernes <amraromoro@hotmail.com>
2024-03-17 06:05:21 +0200

    fix: Fixed the starts and merged all state togather

--- app/(tabs)/evaluation.tsx
+++ app/(tabs)/evaluation.tsx
@@ -1,86 +1,72 @@
-import { Pressable, ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 import { View, Text } from "../../components/Themed";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Button from "../../components/Button";
 import { tintColorSecondary } from "../../constants/Colors";
 import Stars from "../../components/Stars";
-import StarRating from "react-native-star-rating-widget";
 
-const services: string[] = [
-  "Reception",
-  "Cabin",
-  "House Keeping",
-  "Dinning Table",
-  "Food Quality",
-  "Food Quantity",
-  "Food Variety",
-  "Resturant Services",
-  "Bar Services",
-  "Bar Products Variety",
-];
-
-const rates: string[] = ["Bad", "Normal", "Good", "Very Good", "Excellent"];
+type Services = {
+  name: string;
+  rate: string;
+  rateMaxValue: number;
+};
 
 const evaluation = () => {
-  const [currentValue, setCurrentValue] = useState(0);
-  const [ratedServices, setRatedServices] = useState<string[]>([]);
-  const [form, setForm] = useState({
-    Reception: "",
-    Cabin: "",
-    HouseKeeping: "",
-    DinningTable: "",
-    FoodQuality: "",
-    FoodVariety: "",
-    FoodQuantity: "",
-    ResturantServices: "",
-    BarServices: "",
-    BarProductsVariety: "",
-  });
+  const rates: string[] = ["Bad", "Normal", "Good", "Very Good", "Excellent"];
 
+  const [services, setServices] = useState<Services[]>([
+    { name: "Reception", rate: "", rateMaxValue: 0 },
+    { name: "Cabin", rate: "", rateMaxValue: 0 },
+    { name: "House Keeping", rate: "", rateMaxValue: 0 },
+    { name: "Dinning Table", rate: "", rateMaxValue: 0 },
+    { name: "Food Quality", rate: "", rateMaxValue: 0 },
+    { name: "Food Quantity", rate: "", rateMaxValue: 0 },
+    { name: "Food Variety", rate: "", rateMaxValue: 0 },
+    { name: "Restaurant Services", rate: "", rateMaxValue: 0 },
+    { name: "Bar Services", rate: "", rateMaxValue: 0 },
+    { name: "Bar Products Variety", rate: "", rateMaxValue: 0 },
+  ]);
 
-  const handleClick = (value: number, serviceName: string) => {
-    setCurrentValue(value + 1);
-/*     setRatedServices([
-      ...ratedServices,
-      serviceName
-    ]) */
+  const handleClick = (serviceIndex: number, starIndex: number) => {
+    const updatedService = {
+      ...services[serviceIndex],
+      rate: rates[starIndex],
+      rateMaxValue: starIndex + 1,
+    };
+    const newServices = [...services];
+    newServices[serviceIndex] = updatedService;
 
-    setForm((prev) => ({
-      ...prev,
-      [serviceName.replace(/\s+/g, "")]: rates[value],
-    }));
+    setServices(newServices);
   };
-  console.log("After", form);
 
   return (
-    <View>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.container}>
-          <Text style={styles.title}>Evaluation Points</Text>
+    <ScrollView showsVerticalScrollIndicator={false} style={{ marginTop: 35 }}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Evaluation Points</Text>
 
-          {services.map((service, serviceIndex) => (
-            <View key={serviceIndex}>
-              <Text style={styles.itemTitle}>{service}</Text>
-              <View style={styles.starsContainer}>
-                {rates.map((v, index) => (
-                  <Stars
-                    key={index}
-                    value={index}
-                    maxValue={currentValue}
-                    onClick={() => handleClick(index, service)}
-                  />
-                ))}
-              </View>
+        {services.map((service, serviceIndex) => (
+          <View key={serviceIndex}>
+            <Text style={styles.itemTitle}>{service.name}</Text>
+            <View style={styles.starsContainer}>
+              {rates.map((_, starIndex) => (
+                <Stars
+                  key={starIndex}
+                  value={starIndex}
+                  maxValue={services[serviceIndex].rateMaxValue}
+                  onClick={() => handleClick(serviceIndex, starIndex)}
+                />
+              ))}
             </View>
-          ))}
-
-          <View style={styles.buttonContainer}>
-            <Button title="Submit" color={tintColorSecondary} page="home" />
           </View>
+        ))}
+
+        <View style={styles.buttonContainer}>
+          {services[services.length - 1].rateMaxValue !== 0 ? (
+            <Button title="Submit" color={tintColorSecondary} page="home" />
+          ) : null}
         </View>
-      </ScrollView>
-    </View>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -111,14 +97,11 @@ const styles = StyleSheet.create({
     width: "60%",
     display: "flex",
     alignItems: "center",
-    marginVertical: 10,
+    marginVertical: 20,
   },
   starsContainer: {
     display: "flex",
     flexDirection: "row",
-  },
-  star: {
-    padding: 3,
   },
 });
 
--- components/Stars.tsx
+++ components/Stars.tsx
@@ -1,55 +1,24 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
+import { Pressable, StyleSheet } from "react-native";
+import React from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { tintColorSecondary } from "../constants/Colors";
 
 type Props = {
-  name: string;
-};
-
-const Stars = ({
-  value,
-  maxValue,
-  onClick,
-}: {
   value: number;
   maxValue: number;
   onClick(): void;
-}) => {
-  /*   const [currentValue, setCurrentValue] = useState(0);
-  const [form, setForm] = useState({
-    Reception: "",
-    Cabin: "",
-    HouseKeeping: "",
-    DinningTable: "",
-    FoodQuailty: "",
-    FoodVariety: "",
-    ResturantServices: "",
-    BarServices: "",
-    BarProductsVariety: "",
-  });
+};
 
-  const rates: string[] = ["Bad", "Normal", "Good", "Very Good", "Excellent"]; */
-
-  /*   const handleClick = (value: number) => {
-    setCurrentValue(value + 1);
-
-    // Here We Gonna send post request with each rate or the full rate
-    setForm((prev) => ({
-      ...prev,
-      [props.name]: rates[value],
-    }));
-  }; */
-
+const Stars = ({ value, maxValue, onClick }: Props) => {
   return (
-      <Pressable onPress={onClick}>
-        <Ionicons
-          name="ios-star"
-          size={40}
-          color={value < maxValue ? tintColorSecondary : "#cccc"}
-          style={styles.star}
-        />
-      </Pressable>
+    <Pressable onPress={onClick}>
+      <Ionicons
+        name="ios-star"
+        size={40}
+        color={value < maxValue ? tintColorSecondary : "#cccc"}
+        style={styles.star}
+      />
+    </Pressable>
   );
 };
 
