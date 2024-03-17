9f4ec1b77e2f21dbb40ec33e33b66add8a679cb2
AmrViernes <amraromoro@hotmail.com>
2024-03-17 06:05:25 +0200

    refactor: Enhanced the component types and added setLoadingToFalse and setLoadingToTrue

--- app/context/DataContext.tsx
+++ app/context/DataContext.tsx
@@ -8,25 +8,25 @@ import {
   useState,
 } from "react";
 
-type Home_Data = {
+type HomeData = {
   ROOM_NO: string;
   NEXT_EVENT: string;
-  FINANCE: [];
-  PROGRAM: [{
-    PROG_ID: number,
-    PROG_DATE: string,
-    PROG_TIME: string,
-    PROG_TITLE: string,
-    PROG_DESCRIPTION: string
-  }];
-  REQUEST: [{
-    REQ_ID: number,
-    DEPT_NAME: string,
-    DEPT_NUMBER: number,
-    REQ_DESC: string,
-    REQ_STATUS?: string,
-    ITEM_COUNT?: number
-  }];
+  FINANCE: Array<any>;
+  PROGRAM: Array<{
+    PROG_ID: number;
+    PROG_DATE: string;
+    PROG_TIME: string;
+    PROG_TITLE: string;
+    PROG_DESCRIPTION: string;
+  }>;
+  REQUEST: Array<{
+    REQ_ID: number;
+    DEPT_NAME: string;
+    DEPT_NUMBER: number;
+    REQ_DESC: string;
+    REQ_STATUS?: string;
+    ITEM_COUNT?: number;
+  }>;
 };
 
 interface DataContextProps {
@@ -34,64 +34,82 @@ interface DataContextProps {
 }
 
 interface DataContextValue {
-  homeData: Home_Data;
-  updateHomeData: (newData: Home_Data) => void;
+  homeData: HomeData;
+  updateHomeData: (newData: HomeData) => void;
   setLoadingToFalse: () => void;
-  loading: boolean
+  setLoadingToTrue: () => void;
+  loading: boolean;
 }
 
 const DataContext = createContext<DataContextValue | undefined>(undefined);
 
 export const DataProvider: React.FC<DataContextProps> = ({ children }) => {
-  const [homeData, setHomeData] = useState<Home_Data>({
+  const [homeData, setHomeData] = useState<HomeData>({
     ROOM_NO: "",
     NEXT_EVENT: "",
     FINANCE: [],
-    PROGRAM: [{
-      PROG_ID: 0,
-      PROG_TITLE: '',
-      PROG_DATE: '',
-      PROG_DESCRIPTION: '',
-      PROG_TIME: ''
-    }],
-    REQUEST: [{
-      DEPT_NAME: '',
-      DEPT_NUMBER: 0,
-      REQ_ID: 0,
-      REQ_DESC: ''
-    }]
+    PROGRAM: [
+      {
+        PROG_ID: 0,
+        PROG_TITLE: "",
+        PROG_DATE: "",
+        PROG_DESCRIPTION: "",
+        PROG_TIME: "",
+      },
+    ],
+    REQUEST: [
+      {
+        DEPT_NAME: "",
+        DEPT_NUMBER: 0,
+        REQ_ID: 0,
+        REQ_DESC: "",
+      },
+    ],
   });
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    axios
-      .get(
-        "https://actidesk.oracleapexservices.com/apexdbl/boatmob/guest/home/gtData",
-        {
-          params: {
-            P_APPID: 1,
-            P_RCID: 7977,
-            P_LANGCODE: "E",
-          },
-        }
-      )
-      .then((res) => setHomeData(res.data.RESPONSE[0]))
-      .finally(() => setLoading(false));
-      console.log(homeData);
-      
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://actidesk.oracleapexservices.com/apexdbl/boatmob/guest/home/gtData",
+          {
+            params: {
+              P_APPID: 1,
+              P_RCID: 7977,
+              P_LANGCODE: "E",
+            },
+          }
+        );
+        setHomeData(response.data.RESPONSE[0]);
+        console.log(response.data.RESPONSE[0]);
+        
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
   }, [loading]);
 
-  const updateHomeData = (newData: Home_Data) => {
+  const updateHomeData = (newData: HomeData) => {
     setHomeData(newData);
   };
 
   const setLoadingToFalse = () => {
-    setLoading(true)
-  }
+    if (loading) setLoading(false);
+  };
+
+  const setLoadingToTrue = () => {
+    if (!loading) setLoading(true);
+  };
 
   const value: DataContextValue = {
     loading,
     setLoadingToFalse,
+    setLoadingToTrue,
     homeData,
     updateHomeData,
   };
