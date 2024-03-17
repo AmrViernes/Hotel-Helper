f42ab01b91c6a77e541e261bad82918533647f70
AmrViernes <amraromoro@hotmail.com>
2024-03-17 06:05:31 +0200

    refactor: Changed the Data call to be dynamic from user login

--- app/context/DataContext.tsx
+++ app/context/DataContext.tsx
@@ -1,54 +1,50 @@
 // DataContext.js
 import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
 import {
-  createContext,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
-import { DataContextPropsT, DataContextValueT, HomeDataT } from "../../types/types";
+  DataContextPropsT,
+  DataContextValueT,
+  HomeDataT,
+} from "../../types/types";
+import * as secureStore from "expo-secure-store";
+import { AUTH_KEY, useAuth } from "./AuthContext";
 
 const DataContext = createContext<DataContextValueT | undefined>(undefined);
 
 export const DataProvider: React.FC<DataContextPropsT> = ({ children }) => {
-  const [homeData, setHomeData] = useState<HomeDataT>({
-    ROOM_NO: "",
-    NEXT_EVENT: "",
-    FINANCE: [],
-    PROGRAM: [],
-    REQUEST: [],
-  });
   const [loading, setLoading] = useState<boolean>(true);
-
+  const [homeData, setHomeData] = useState<HomeDataT>();
+  const {authState} = useAuth()
   useEffect(() => {
-    const abort = new AbortController()
+    const abort = new AbortController();
 
     const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://actidesk.oracleapexservices.com/apexdbl/boatmob/guest/home/gtData",
-          {
-            signal: abort.signal,
-            params: {
-              P_APPID: 1,
-              P_RCID: 7977,
-              P_LANGCODE: "E",
-            },
-          }
-        );
-        setHomeData(response.data.RESPONSE[0]);
-        console.log(homeData);
-        
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
-      }
+      const gettingAuth = await secureStore.getItemAsync(AUTH_KEY);
+      const authData = JSON.parse(gettingAuth as string);
+      console.log(authState?.RC_ID)
+        try {
+          const response = await axios.get(
+            "https://actidesk.oracleapexservices.com/apexdbl/boatmob/guest/home/gtData",
+            {
+              params: {
+                P_APPID: 1,
+                P_RCID: authData?.RC_ID || authState?.RC_ID,
+                P_LANGCODE: "E",
+              },
+            }
+          );
+          setHomeData(response.data.RESPONSE[0]);
+          console.log("Home Data Context Triggered");
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        } finally {
+          setLoading(false);
+        }
     };
 
     fetchData();
-    return () => abort.abort(); 
-  }, [loading]);
+    return () => abort.abort();
+  }, [loading, authState]);
 
   const updateHomeData = (newData: HomeDataT) => {
     setHomeData(newData);
