607eda3a8ca95352f8c0a7c8a87a6ad8807364d1
AmrViernes <amraromoro@hotmail.com>
2024-03-17 06:05:30 +0200

    refactor: Cahanged the styling of change password screen

--- app/ChangePassword.tsx
+++ app/ChangePassword.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { StyleSheet } from "react-native";
+import React from "react";
+import { Text, View } from "../components/Themed";
+import { Stack } from "expo-router";
+import { tintColorPrimary, tintColorWarmBackground } from "../constants/Colors";
 
 const ChangePassword = () => {
   return (
-    <div>ChangePassword</div>
-  )
-}
+    <View style={styles.container}>
+          <Stack.Screen
+            options={{
+              headerTitle: "",
+              headerShadowVisible: false,
+              headerStyle: {
+                backgroundColor: tintColorWarmBackground
+              },
+              headerTintColor: tintColorPrimary
+            }}
+          />
+      <Text>ChangePassword</Text>
+    </View>
+  );
+};
 
-export default ChangePassword
+export default ChangePassword;
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+  },
+});
