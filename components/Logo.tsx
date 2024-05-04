02bc640a4a11debaf9b9e3caab58bbb5db31f606
AmrViernes <amraromoro@hotmail.com>
2024-05-04 19:11:20 +0300

    refactor: linting

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,13 +1,17 @@
-import { View, Text } from './Themed'
-import React from 'react'
+import { View, Text } from "./Themed";
+import React from "react";
 import { Image } from "expo-image";
 
 const Logo = () => {
   return (
-    <View style={{marginLeft: 15}}>
-      <Image source={require('../assets/images/Logo.png')} contentFit='fill' style={{width: 90 , height: 70}}/>
+    <View style={{ marginLeft: 15 }}>
+      <Image
+        source={require("../assets/images/Logo.png")}
+        contentFit="fill"
+        style={{ width: 90, height: 70 }}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
