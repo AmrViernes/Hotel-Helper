29aef07bfcb74bc83296348086493d402db812df
AmrViernes <amraromoro@hotmail.com>
2024-05-05 23:23:08 +0300

    refactor: indent

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,22 +1,22 @@
 export const tintColorPrimary = "#466d86";
 export const tintColorSecondary = "#e5a134";
-export const tintColorWarmBackground = '#FCF5ED'
-export const tintColorColdBackground = '#DFF6FF'
-export const tintColorDisabled = "#f5ebe0"
+export const tintColorWarmBackground = "#FCF5ED";
+export const tintColorColdBackground = "#DFF6FF";
+export const tintColorDisabled = "#f5ebe0";
 
 export default {
   light: {
     text: tintColorPrimary,
-    iconOutLine: '#fff',
+    iconOutLine: "#fff",
     background: tintColorWarmBackground,
     tint: tintColorPrimary,
     tabIconDefault: "#fff",
     tabIconSelected: tintColorPrimary,
   },
   dark: {
-    text: '#fff',
-    iconOutLine: '#fff',
-    background: '#fffff',
+    text: "#fff",
+    iconOutLine: "#fff",
+    background: "#fffff",
     tint: tintColorSecondary,
     tabIconDefault: "#fff",
     tabIconSelected: tintColorPrimary,
